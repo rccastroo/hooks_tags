6d691a2c3943d57b7851edca86e144a40958e3e6
rccastroo <rj_ruan@hotmail.com>
2024-08-14 07:29:03 -0300

    Up data

--- PostgreSQL@localhost - customers 2024-08-06 10-31-58.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO public.customers ("name",email,"password",phone,active,last_login) VALUES
-	 ('Ruan Carlos','rj_ruan@hotmail.com','$2a$12$bjzU02Z/JO/spMJ6JjRkCO0Ezh5vE6VVLIGCN6lsfOKWtb/2Uahri','+55 21 99443-4501',true,'2024-08-06 00:29:13.152589');
